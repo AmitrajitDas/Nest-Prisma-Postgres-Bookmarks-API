95f4c9e9bb8f304a5e083e844991db003559b550
AmitrajitDas <amitrajitdas31@gmail.com>
2023-01-05 23:54:23 +0530

    feat: bookmark dtos

---
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsString, IsNotEmpty } from 'class-validator';
+
+export class EditBookmarkDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  link?: string;
+}
